d3137d7a6794cfb4e499dea5fba791abe4e30f91
Darken141 <Darken141@github.com>
2020-05-26 15:28:50 +0200

    change heading

--- src/context/Hero-section.js
+++ src/context/Hero-section.js
@@ -3,7 +3,7 @@ import React, { createContext } from 'react';
 export const HeroSectionContext = createContext({
 	lang: 'sk',
 	SEOTitle: 'Domovská stránka',
-	heading: 'Video software pre zber odozvy.',
+	heading: 'Video kolaborácia a zber odozvy.',
 	subHeading: 'Checkvid.io ti umožní efektivne zberať feedback od tvojich klientov a tým ti zjednodušiť prácu.',
 	CTA: 'CHCEM BYŤ INFORMOVANÝ'
 });
