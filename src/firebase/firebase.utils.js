5a0ea421d1cc90d2f1d31fa3d52297d63b7a93cd
Darken141 <Darken141@github.com>
2020-05-27 02:30:28 +0200

    fix dotenv

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,39 +2,39 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import { store } from 'react-notifications-component';
 
-import {
-	apiKey,
-	appId,
-	authDomain,
-	databaseURL,
-	measurementId,
-	messagingSenderId,
-	projectId,
-	storageBucket
-} from '../../env/index';
-
-const firebaseConfig = {
-	apiKey: apiKey,
-	authDomain: authDomain,
-	databaseURL: databaseURL,
-	projectId: projectId,
-	storageBucket: storageBucket,
-	messagingSenderId: messagingSenderId,
-	appId: appId,
-	measurementId: measurementId
-};
-// console.log(process.env.GATSBY_API_KEY);
+// import {
+// 	apiKey,
+// 	appId,
+// 	authDomain,
+// 	databaseURL,
+// 	measurementId,
+// 	messagingSenderId,
+// 	projectId,
+// 	storageBucket
+// } from '../../env/index';
 
 // const firebaseConfig = {
-// 	apiKey: process.env.GATSBY_API_KEY,
-// 	authDomain: process.env.GATSBY_AUTH_DOMAIN,
-// 	databaseURL: process.env.GATSBY_DATABASE_URL,
-// 	projectId: process.env.GATSBY_PROJECT_ID,
-// 	storageBucket: process.env.GATSBY_STORAGE_BUCKET,
-// 	messagingSenderId: process.env.GATSBY_MESSAGING_SENDER_ID,
-// 	appId: process.env.GATSBY_APP_ID,
-// 	measurementId: process.env.GATSBY_MEASUREMENT_ID
+// 	apiKey: apiKey,
+// 	authDomain: authDomain,
+// 	databaseURL: databaseURL,
+// 	projectId: projectId,
+// 	storageBucket: storageBucket,
+// 	messagingSenderId: messagingSenderId,
+// 	appId: appId,
+// 	measurementId: measurementId
 // };
+// console.log(process.env.GATSBY_API_KEY);
+
+const firebaseConfig = {
+	apiKey: process.env.GATSBY_API_KEY,
+	authDomain: process.env.GATSBY_AUTH_DOMAIN,
+	databaseURL: process.env.GATSBY_DATABASE_URL,
+	projectId: process.env.GATSBY_PROJECT_ID,
+	storageBucket: process.env.GATSBY_STORAGE_BUCKET,
+	messagingSenderId: process.env.GATSBY_MESSAGING_SENDER_ID,
+	appId: process.env.GATSBY_APP_ID,
+	measurementId: process.env.GATSBY_MEASUREMENT_ID
+};
 
 export const addEmailToList = async (email) => {
 	if (!email) return;
